c6ace23e4de9d81d8f781ecf4eb09dcbbd0ec107
Alisson Lima <alisson.lima@simplesdental.com>
2024-11-20 18:23:02 -0300

    Implementado dos testes para tratamentos dos dados de Estabelecimentos

--- dags/transform_dados_abertos_cnpj.py
+++ dags/transform_dados_abertos_cnpj.py
@@ -1,9 +1,8 @@
 """
-Realiza o processo completo de web scrapping da pagina de links de
-CNPJs da receita federal e salva os dados em um banco de dados PostgreSQL
-para serem utilizados em analises
+Realiza o processo de descompactacao do arquivos ZIP com dados
+abertos do Ministerio da Fazendo que foram baixados pelo processo download_dados_abertos
 """
-
+import logging
 import os
 import re
 import zipfile
@@ -23,6 +22,29 @@ ROOT_URL = "https://arquivos.receitafederal.gov.br/dados/cnpj/dados_abertos_cnpj
 DATA_OUTPUT_DIR = "/opt/airflow/data"
 
 
+@task()
+def extract_zip_files():
+    # Percorre todos os arquivos no diretório
+    for file_name in os.listdir(DATA_OUTPUT_DIR):
+        logging.info(f"Extracing file {file_name}")
+        if file_name.endswith(".zip"):
+            # Caminho completo do arquivo .zip
+            zip_file_path = os.path.join(DATA_OUTPUT_DIR, file_name)
+
+            # Usa regex para remover números e extensão do nome do arquivo
+            base_name = re.sub(r'\d*\.zip$', '', file_name)
+            output_folder = os.path.join(DATA_OUTPUT_DIR, base_name)
+
+            # Cria o diretório de saída se ele não existir
+            os.makedirs(output_folder, exist_ok=True)
+
+            # Descompacta o arquivo .zip para o diretório de saída
+            with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+                print(f"Unziping {file_name} to {output_folder}...")
+                zip_ref.extractall(output_folder)
+                print(f"{file_name} unziped successfully!")
+
+
 @dag(
     schedule=[DS_DADOS_ABERTOS_CNPJ],
     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
@@ -35,11 +57,7 @@ DATA_OUTPUT_DIR = "/opt/airflow/data"
     }
 )
 def transform_dados_abertos_cnpj():
-    @task()
-    def print_start():
-        print('Iniciando o processo de download dos dados abertos de CNPJs')
-
-    print_start()
+    extract_zip_files()
 
 
 dag = transform_dados_abertos_cnpj()
